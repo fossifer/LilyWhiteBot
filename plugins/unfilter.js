590866da286f833109400e8b395ee24012e9745c
mrhso <liu.songquan@live.cn>
2018-10-29 20:17:25 +0800

    unfilter

---
+++ plugins/unfilter.js
@@ -0,0 +1,62 @@
+/*
+ * unfilter.js 過濾不符合特定規則的訊息
+ *
+ * 使用方法同 filter.js。
+ */
+
+'use strict';
+
+const BridgeMsg = require('./transport/BridgeMsg.js');
+
+let msgfilters = {
+    send: [],
+    receive: []
+};
+
+module.exports = (pluginManager, options) => {
+    const bridge = pluginManager.plugins.transport;
+
+    if (!bridge) {
+        return;
+    }
+
+    let filters = options.filters || [];
+    for (let f of filters) {
+        let arr, opt = {};
+        if (f.event === 'receive') {
+            arr = msgfilters.receive;
+        } else if (f.event === 'send' || !f.event) {
+            arr = msgfilters.send;
+        } else {
+            continue;
+        }
+
+        if (f.from !== undefined) { opt.from_uid = f.from; }
+        if (f.to   !== undefined) { opt.to_uid   = f.to; }
+        if (f.nick !== undefined) { opt.nick     = f.nick; }
+        if (f.text !== undefined) { opt.text     = f.text; }
+
+        arr.push(opt);
+    }
+
+    const process = (event) => (msg) => {
+        let filters = msgfilters[event];
+
+        for (let f of filters) {
+            let rejects = false;
+            for (let prop in f) {
+                if (!(msg[prop] && msg[prop].toString().match(f[prop]))) {
+                    rejects = true;
+                    break;
+                }
+            }
+            if (rejects) {
+                return Promise.reject();
+            }
+        }
+    };
+
+    bridge.addHook('bridge.send', process('send'));
+    bridge.addHook('bridge.receive', process('receive'));
+};
+
