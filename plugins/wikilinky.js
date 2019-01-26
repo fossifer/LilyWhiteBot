37f96283ba054e9cb257ee323d3095f66358d9f0
mrhso <liu.songquan@live.cn>
2019-01-26 09:20:19 +0800

    修正使用例

--- plugins/wikilinky.js
+++ plugins/wikilinky.js
@@ -7,9 +7,7 @@
 
     "wikilinky": {
         "groups": [
-            {
-                "qq/123123123": "https://zh.wikipedia.org/wiki/$1"
-            }
+            "qq/123123123": "https://zh.wikipedia.org/wiki/$1"
         ]
     }
  */
@@ -119,7 +117,7 @@ module.exports = (pluginManager, options) => {
         map[type] = {};
     }
 
-    let groups = options.groups || {};
+    let groups = options.groups || [];
     for (let group in groups) {
         let client = BridgeMsg.parseUID(group);
         if (client.uid) {
