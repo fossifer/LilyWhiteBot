d18b77ad8e78da23133329c2bca13ab3a982c6ec
infnan <38274826+infnan@users.noreply.github.com>
2020-05-19 13:58:09 +0800

    fix

--- plugins/transport/paeeye.js
+++ plugins/transport/paeeye.js
@@ -4,17 +4,28 @@ const winston = require('winston');
 
 module.exports = (bridge, options) => {
     bridge.addHook('bridge.send', (msg) => new Promise((resolve, reject) => {
-        if (options.options.paeeye && (options.options.paeeye.prepend || options.options.paeeye.prepend)) {
-            if (msg.text.startsWith(options.options.paeeye.prepend) ||
-                msg.text.includes(options.options.paeeye.inline)) {
-                winston.debug(`[paeeye.js] #${msg.msgId}: Ignored.`);
-                reject(false);
-                return;
-            } else if (msg.extra.reply && (msg.extra.reply.message.startsWith(options.options.paeeye.prepend) ||
-                msg.extra.reply.message.includes(options.options.paeeye.inline))) {
-                winston.debug(`[paeeye.js] #${msg.msgId}: Ignored.`);
-                reject(false);
-                return;
+        let paeeye = options.options.paeeye;
+
+        if (paeeye) {
+            if (typeof paeeye === 'string') {
+                if (msg.text.startsWith(paeeye) ||
+                    (msg.extra.reply && msg.extra.reply.message.startsWith(paeeye))) {
+                    winston.debug(`[paeeye.js] #${msg.msgId}: Ignored.`);
+                    reject(false);
+                    return;
+                }
+            } else {
+                if (msg.text.startsWith(paeeye.prepend) ||
+                    msg.text.includes(paeeye.inline)) {
+                    winston.debug(`[paeeye.js] #${msg.msgId}: Ignored.`);
+                    reject(false);
+                    return;
+                } else if (msg.extra.reply && (msg.extra.reply.message.startsWith(paeeye.prepend) ||
+                    msg.extra.reply.message.includes(paeeye.inline))) {
+                    winston.debug(`[paeeye.js] #${msg.msgId}: Ignored.`);
+                    reject(false);
+                    return;
+                }
             }
         }
         resolve();
