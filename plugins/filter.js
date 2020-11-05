c869998acb86c7f858fb5c1aab794549f0855637
lziad <23732689+lziad@users.noreply.github.com>
2020-11-05 17:18:29 +0800

    中文维基互联群所使用的转发机器人 zhmrtbot 目前进行的魔改

--- plugins/filter.js
+++ plugins/filter.js
@@ -58,13 +58,28 @@ module.exports = (pluginManager, options) => {
 
         for (let f of filters) {
             let rejects = true;
+			let rejects_reply = false;
             for (let prop in f) {
                 if (!(msg[prop] && msg[prop].toString().match(f[prop]))) {
                     rejects = false;
                     break;
                 }
             }
-            if (rejects) {
+			// p4: also filter the replied message
+			if (msg.extra.reply) {
+				rejects_reply = true;
+				let reply = msg.extra.reply;
+				reply.text = reply.message;
+				reply.to_uid = msg.to_uid;
+				reply.from_uid = msg.from_uid;
+				for (let prop in f) {
+					if (!(reply[prop] && reply[prop].toString().match(f[prop]))) {
+						rejects_reply = false;
+						break;
+					}
+				}
+			}
+            if (rejects || rejects_reply) {
                 return Promise.reject();
             }
         }
