7404d9946e9ddb3a35ed3fa514fa8080a60babbc
Joch2520 <20432565+Joch2520@users.noreply.github.com>
2021-04-05 08:11:37 +0800

    增加qq查詢合併轉發消息內容插件

---
+++ plugins/qqmultimsg.js
@@ -0,0 +1,70 @@
+/*
+* Get qq multi forward messages
+*
+* command: '!qmulti'
+* types: [
+*     'qq/123456'
+* ]
+*/
+
+'use strict';
+
+const format = require('string-format');
+
+module.exports = (pluginManager, options) => {
+    const bridge = pluginManager.plugins.transport;
+
+    if (!bridge || !pluginManager.handlers.has('QQ')) {
+        return;
+    }
+
+    let command = options.command || '!qmulti';
+
+    bridge.addCommand(command, (context) => {
+        if (context.isPrivate) {
+          if (['Telegram', 'IRC', 'Discord'].includes(context._from_client)) {
+              let handler = pluginManager.handlers.get(context._from_client);
+              let qqHandler = pluginManager.handlers.get('QQ');
+              qqHandler._client.getForwardMsg(context.param.split(' ')[0]).then(res=>{
+                if (res.status=='ok') {
+                  // console.log(res.data)
+                  res.data.forEach(msg=>{
+                    let message = qqHandler.parseMessage(msg.message);
+                    let meta = {
+                        nick: msg.nickname||msg.user_id,
+                        from: msg.user_id,
+                        to: msg.group_id,
+                        text: message.text,
+                        client_short: 'Q',
+                        client_full: 'QQ',
+                    };
+                    
+                    
+                    if (message.extra.multimsg) {
+                      meta.text+=`\n[私聊机器人使用 !qmulti ${message.extra.multimsg[0]} 以${message.extra.multimsg[1]}]`;
+                    }
+                    
+                    let output = format('[{nick}] {text}', meta);
+                    
+                    // 处理图片和音频附件
+                    for (let file of message.extra.images) {
+                        output += '\n' + file;
+                    }
+                    for (let file of message.extra.records) {
+                        output += '\n' + file;
+                    }
+                    for (let file of message.extra.videos) {
+                        output += '\n' + file;
+                    }
+                    
+                    handler.say(context.to, output)
+                  })
+                } else {
+                  handler.say(context.to, `找不到指定的合并转发消息。`);
+                }
+              });
+              
+          }
+        }
+    }, options);
+};
